a28b5350c9babede40001f853fb797bad260530d
sakamoto <sakamoto@cdcdaq.(none)>
2016-02-26 07:48:38 +0900

    add +O at .L XXX.cc+O

--- compile.C
+++ compile.C
@@ -1,14 +1,11 @@
 {
-   //gROOT->ProcessLine(".L wire.cc+");
-   gROOT->ProcessLine(".L line.cc+");
-   gROOT->ProcessLine(".L wiremap.cc+");
-   gROOT->ProcessLine(".L event.cc+");
-   gROOT->ProcessLine(".L xtcurve.cc+");
-   gROOT->ProcessLine(".L hit.cc+");
-   gROOT->ProcessLine(".L track.cc+");
-   gROOT->ProcessLine(".L chamber.cc+");
-   gROOT->ProcessLine(".L trackfinder.cc+");
-   gROOT->ProcessLine(".L run.cc+");
-   //gROOT->ProcessLine(".L run.cc+");
-
+   gROOT->ProcessLine(".L line.cc+O");
+   gROOT->ProcessLine(".L wiremap.cc+O");
+   gROOT->ProcessLine(".L event.cc+O");
+   gROOT->ProcessLine(".L xtcurve.cc+O");
+   gROOT->ProcessLine(".L hit.cc+O");
+   gROOT->ProcessLine(".L track.cc+O");
+   gROOT->ProcessLine(".L chamber.cc+O");
+   gROOT->ProcessLine(".L trackfinder.cc+O");
+   gROOT->ProcessLine(".L run.cc+O");
 }
