0075de33b0e5f83c18cecb60350e330c779167b8
sakah <h0.sakamoto@gmail.com>
2016-02-29 05:34:55 +0900

    update AnaResRVSHitR. Add peak histograms

--- analysis.cc
+++ analysis.cc
@@ -31,12 +31,19 @@ void Analysis::Loop(int max_events)
    if (max_events>num_ents) {
       max_events = num_ents;
    }
+   num_events_ = 0;
    for (int iev=0; iev<max_events; iev++) {
       outroot_.GetEntry(iev);
       if (!LoopEvent()) continue;
       //outroot_.PrintEntry();
+      num_events_++;
    }
    EndOfEvent();
+}
+
+int Analysis::GetNumEvents()
+{
+   return num_events_;
 }
 
 void Analysis::BeginOfEvent()
@@ -65,6 +72,9 @@ void AnaResRVSHitR::BeginOfEvent()
 {
    for (int test_cid=0; test_cid<MAX_LAYER; test_cid++) {
       h2_resR_VS_hitR_[test_cid] = new TH2F(Form("%s_resR_VS_hitR_%d", name_, test_cid), "", 100, 0, 10, 100, -2, 2);
+      for (int ihitR=0; ihitR<20; ihitR++) {
+         h1_resR_[test_cid][ihitR] = new TH1F(Form("%s_resR_%d_%d", name_, test_cid, ihitR), "", 100, -2, 2);
+      }
    }
 }
 
@@ -74,6 +84,12 @@ bool AnaResRVSHitR::LoopEvent()
       double hitR = outroot_.GetTrackHitR(test_cid, test_cid);
       double resR = outroot_.GetTrackResR(test_cid, test_cid);
       h2_resR_VS_hitR_[test_cid]->Fill(hitR, resR);
+
+      int ihitR = 0;
+      for (int i=0; i<20; i++) {
+         if (hitR>=i*0.5 && hitR<(i+1)*0.5) ihitR=i;
+      }
+      h1_resR_[test_cid][ihitR]->Fill(resR);
    }
    return true;
 }
@@ -82,10 +98,20 @@ void AnaResRVSHitR::EndOfEvent()
 {
    for (int test_cid=1; test_cid<MAX_LAYER-1; test_cid++) {
       double x[20];
+      double y1[20];
+      double y2[20];
       for (int ihitR=0; ihitR<20; ihitR++) {
          x[ihitR] = ihitR*0.5;
+         TH1F* h1 = h1_resR_[test_cid][ihitR];
+         y1[ihitR] = h1->GetMean();
+         int imax = h1->GetMaximumBin();
+         y2[ihitR] = h1->GetXaxis()->GetBinCenter(imax);
       }
-      gr_resR_mean_VS_hitR_[test_cid] = new TGraph(20, x, resR_mean_[test_cid]);
+      gr_resR_mean_VS_hitR_[test_cid] = new TGraph(20, x, y1);
+      gr_resR_peak_VS_hitR_[test_cid] = new TGraph(20, x, y2);
+
+      gr_resR_mean_VS_hitR_[test_cid]->SetMarkerStyle(20);
+      gr_resR_peak_VS_hitR_[test_cid]->SetMarkerStyle(20);
    }
 } 
 
@@ -94,7 +120,29 @@ TH2F* AnaResRVSHitR::GetResRVSHitR(int test_cid)
    return h2_resR_VS_hitR_[test_cid];
 }
 
-TGraph* AnaResRVSHitR::GetResRMeanVSHitR(int test_cid)
+TH1F* AnaResRVSHitR::GetResR(int test_cid, int ihitR)
+{
+   return h1_resR_[test_cid][ihitR];
+}
+
+TGraph* AnaResRVSHitR::GetResRVSHitRMean(int test_cid)
 {
    return gr_resR_mean_VS_hitR_[test_cid];
+}
+
+TGraph* AnaResRVSHitR::GetResRVSHitRPeak(int test_cid)
+{
+   return gr_resR_peak_VS_hitR_[test_cid];
+}
+
+void AnaResRVSHitR::DrawResRVSHitRMean(int test_cid)
+{
+   GetResRVSHitR(test_cid)->Draw("colz");
+   GetResRVSHitRMean(test_cid)->Draw("pl same");
+}
+
+void AnaResRVSHitR::DrawResRVSHitRPeak(int test_cid)
+{
+   GetResRVSHitR(test_cid)->Draw("colz");
+   GetResRVSHitRPeak(test_cid)->Draw("pl same");
 }
--- analysis.h
+++ analysis.h
@@ -14,6 +14,7 @@ class Analysis
       Analysis();
       void OpenRootFile(const char* path);
       void Loop(int max_events=2147483647);
+      int GetNumEvents();
       virtual void BeginOfEvent();
       virtual bool LoopEvent();
       virtual void EndOfEvent();
@@ -21,6 +22,9 @@ class Analysis
    protected:
       OutputROOT outroot_;
       char name_[128]; // set uniq name according to path
+
+   private:
+      int num_events_; // total number of analized events
 };
 
 class AnaResRVSHitR : public Analysis
@@ -31,12 +35,17 @@ class AnaResRVSHitR : public Analysis
       virtual bool LoopEvent();
       virtual void EndOfEvent();
       TH2F* GetResRVSHitR(int test_cid);
-      TGraph* GetResRMeanVSHitR(int test_cid);
+      TH1F* GetResR(int test_cid, int ihitR);
+      TGraph* GetResRVSHitRMean(int test_cid);
+      TGraph* GetResRVSHitRPeak(int test_cid);
+      void DrawResRVSHitRMean(int test_cid);
+      void DrawResRVSHitRPeak(int test_cid);
 
    private:
       TH2F* h2_resR_VS_hitR_[MAX_LAYER]; // [test_cid]
+      TH1F* h1_resR_[MAX_LAYER][20]; // [test_cid]
       TGraph* gr_resR_mean_VS_hitR_[MAX_LAYER];
-      double resR_mean_[MAX_LAYER][20];  // [test_cid][ihitR]
+      TGraph* gr_resR_peak_VS_hitR_[MAX_LAYER];
 };
 
 #endif
