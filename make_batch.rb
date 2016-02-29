8a9da149ba26e8f01d421abb87adb3b0fb67508f
sakah <h0.sakamoto@gmail.com>
2016-02-29 23:09:58 +0900

    modify make_batch.rb. Add config number in output's root

--- make_batch.rb
+++ make_batch.rb
@@ -3,13 +3,15 @@
 require "fileutils"
 
 $inroot_dir="/home/had/hideyuki/private/genfit2/KEKCC/root/cosmic_root/"
-$outroot_dir="/home/had/hideyuki/private/genfit2/KEKCC/root/cosmic_root_out/freet0/sub/"
+$outroot_dir="/home/had/hideyuki/private/genfit2/KEKCC/root/cosmic_root_out/sub/"
 $prog_dir="/home/had/hideyuki/private/genfit2/KEKCC/hs_cosmic/"
+$config_dir="/home/had/hideyuki/private/genfit2/KEKCC/hs_cosmic/config/"
 
-def command(total, run_num, fit_func_name)
+def command(total, run_num, config_num)
    #Usage ./main <input.root> <output.root> <start_event> <last_event> <fit_func_name>
    num_each=10000
    input_root="run#{run_num}.root"
+   config_path="#{$config_dir}/config#{config_num}.txt"
    0.step(total, num_each) do |iev|
       iev_str="%07d"%iev
       start_iev=iev
@@ -17,17 +19,21 @@ def command(total, run_num, fit_func_name)
       if last_iev > total-1
          last_iev = total - 1
       end
-      output_root="run_#{run_num}_#{iev_str}.root"
+      output_root="run_#{run_num}_config_#{config_num}_#{iev_str}.root"
       #puts "iev #{start_iev} #{last_iev}"
       cmd="#!/bin/bash\n"
-      cmd+="#{$prog_dir}/main #{$inroot_dir}/#{input_root} #{$outroot_dir}/#{output_root} #{start_iev} #{last_iev} #{fit_func_name}\n"
-      File.open("batch/run_#{run_num}_#{iev_str}.sh", "w") do |file|
+      cmd+="#{$prog_dir}/main #{$inroot_dir}/#{input_root} #{$outroot_dir}/#{output_root} #{start_iev} #{last_iev} #{config_path}\n"
+      File.open("batch/run_#{run_num}_config_#{config_num}_#{iev_str}.sh", "w") do |file|
          file.write cmd
       end
-      FileUtils.chmod(0755,"batch/run_#{run_num}_#{iev_str}.sh")
+      FileUtils.chmod(0755,"batch/run_#{run_num}_config_#{config_num}_#{iev_str}.sh")
    end
 end
 
-command(39322,   16, "FIT_FUNC_TYPE_FREE_T0")
-command(1730623, 24, "FIT_FUNC_TYPE_FREE_T0")
-command(2114226, 27, "FIT_FUNC_TYPE_FREE_T0")
+command(39322,   16, 1);
+command(1730623, 24, 1);
+command(2114226, 27, 1);
+
+command(39322,   16, 2);
+command(1730623, 24, 2);
+command(2114226, 27, 2);
