d09746f227a71d1a2ea1ce4364328500ff046ed0
sakamoto <h0.sakamoto@gmail.com>
2016-03-03 07:00:18 +0900

    update alias.C

--- alias.C
+++ alias.C
@@ -1,9 +1,9 @@
 {
    for (int cid=0; cid<9; cid++) {
       for (int cid=0; cid<9; cid++) {
-         t->SetAlias(Form("track_resR[%d][%d]",cid,cid), 
-               Form("abs(track_hitR[%d][%d])-track_fitX[%d][%d]", cid,cid,cid,cid));
-         t->SetAlias(Form("track_hitdT[%d][%d]",cid,cid), 
+         t->SetAlias(Form("track_resR_%d_%d",cid,cid), 
+               Form("abs(track_fitX[%d][%d])-track_hitR[%d][%d]", cid,cid,cid,cid));
+         t->SetAlias(Form("track_hitdT_%d_%d",cid,cid), 
                Form("track_hitT[%d][%d]-track_fitT0[%d][%d]", cid,cid,cid,cid));
       }
    }
