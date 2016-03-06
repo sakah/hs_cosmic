cc861d1b3aa09c46111422e34250c15bc6e859f6
sakah <h0.sakamoto@gmail.com>
2016-03-06 22:38:05 +0900

    move scripts for KEKCC

---
+++ script/kekcc/download_cosmic_root_from_kunosrv.sh
@@ -0,0 +1,1 @@
+rsync -avzP -e 'ssh sakamoto@133.1.141.121 ssh' 192.168.2.30:/data3/cdcOsaka/root/ /gpfs/home/had/hideyuki/private/genfit2/KEKCC/root/
---
+++ script/kekcc/upload_cosmic_root_out_to_kunosrv.sh
@@ -0,0 +1,1 @@
+rsync -avzuP /gpfs/home/had/hideyuki/private/genfit2/KEKCC/root/cosmic_root_out/ --exclude=cosmic_root_out/sub/ -e 'ssh sakamoto@133.1.141.121 ssh' sakamoto@192.168.2.30:/home/sakamoto/hs_cosmic/cosmic_root_out/
