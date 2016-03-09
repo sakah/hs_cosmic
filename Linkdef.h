fd37dfc1dec5b4f40e86ee5ed215dc18e4321a0b
sakah <h0.sakamoto@gmail.com>
2016-03-10 06:18:01 +0900

    Add Linkdef.h for rootcint

---
+++ Linkdef.h
@@ -0,0 +1,22 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class Cell+;
+#pragma link C++ class Chamber+;
+#pragma link C++ class Config+;
+#pragma link C++ class ConfigList+;
+#pragma link C++ class Event+;
+#pragma link C++ class Hit+;
+#pragma link C++ class IputROOT+;
+#pragma link C++ class Layer+;
+#pragma link C++ class Line+;
+#pragma link C++ class Range+;
+#pragma link C++ class Run+;
+#pragma link C++ class TrackFindRange+;
+#pragma link C++ class XTcurve+;
+#pragma link C++ class WireMap+;
+
+#endif
